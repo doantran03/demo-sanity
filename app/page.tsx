625f00f74f446613bea1e24a81fa627df3b15b65
doantran03 <doan.tran@py-media.com>
2026-04-02 08:40:33 +0700

    debug query

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,9 @@ export interface Member {
   pids?: string[];
 }
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export default async function Home() {
   const members: Member[] = await client.fetch(
     `*[_type == "member"]{
